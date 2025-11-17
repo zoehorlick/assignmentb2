8a7c780b81025984ed200d3507a1c7b45c2431f2
“zoehorlick” <zoehorlick@icloud.com>
2025-11-17 11:14:49 -0800

    Committ DESCRIPTION which includes LICENSE and LICENSE.md

--- R/count_all_missing_by_group.R
+++ R/count_all_missing_by_group.R
@@ -37,7 +37,8 @@ count_all_missing_by_group <- function(data, group_col, .groups = "drop") {
     }
   }
 
-  data |> group_by({{ group_col }}) |>
+  data |>
+    group_by({{ group_col }}) |>
     summarize(across(everything(), ~sum(is.na(.x))),
               .groups = .groups)
 }
